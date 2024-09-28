0f92b4b558d6405028fca9f81e819d3d132b3fd3
osoki <osokinkonstantin@mail.ru>
2024-09-28 18:11:50 +0300

    start

---
+++ src/test/java/Book/Employee/EmployeesBook/EmployeesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package Book.Employee.EmployeesBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
