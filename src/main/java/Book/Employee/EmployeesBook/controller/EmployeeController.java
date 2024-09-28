d7bcc325ef25dc0491639a3fe20ae439ab91f1aa
osoki <osokinkonstantin@mail.ru>
2024-09-28 19:17:06 +0300

    лист сотрудников/исключения

---
+++ src/main/java/Book/Employee/EmployeesBook/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package Book.Employee.EmployeesBook.controller;
+
+import Book.Employee.EmployeesBook.model.Employee;
+import Book.Employee.EmployeesBook.model.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstName, String lastName) {
+        return service.add(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstName, String lastName) {
+        return service.remove(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam String firstName, String lastName) {
+        return service.find(firstName, lastName);
+    }
+
+    @GetMapping
+    public Collection<Employee> findAll() {
+        return service.findAll();
+    }
+
+
+
+
+
+}
